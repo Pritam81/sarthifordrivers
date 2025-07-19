3ac66d4420c2eb47aff868ecd8efa9c5f3cdc21a
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-07-19 19:04:12 +0530

    Colors.dart updated

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
 var primaryColor = Color.fromARGB(255, 39, 88, 173);
+const Color cardBackgroundColor = Color(0xFFF9FAFC);
+//const Color cardBackgroundColor = Color.fromARGB(255, 224, 231, 243);
+const Color darkTextColor = Color(0xFF212121);
+const Color greyTextColor = Color(0xFF777A7F);
+const double spacing = 12;
